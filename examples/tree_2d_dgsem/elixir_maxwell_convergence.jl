07ce65e56c920e293ac53b8baf9b0c80d0657bdf
fechtner <fechtner@LTN055.mi.uni-koeln.de>
2022-11-30 12:27:57 +0100

    convergence initial condition

--- examples/tree_2d_dgsem/elixir_maxwell_convergence.jl
+++ examples/tree_2d_dgsem/elixir_maxwell_convergence.jl
@@ -7,19 +7,18 @@ using Trixi
 
 flux = FluxLaxFriedrichs(min_max_speed_naive)
 equation = MaxwellEquations2D()
-mesh = TreeMesh((0.0, 0.0), (1.0, 1.0), initial_refinement_level=4, n_cells_max=10^4)
+mesh = TreeMesh((-pi, -pi), (pi, pi), initial_refinement_level=4, n_cells_max=10^4)
 solver = DGSEM(3, flux)
-semi = SemidiscretizationHyperbolic(mesh, equation, initial_condition_test, solver)
+semi = SemidiscretizationHyperbolic(mesh, equation, Trixi.initial_condition_free_stream_conversion, solver)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0,1e-6)
+tspan = (0.0,1*2.0*pi/299792458.0)
 ode = semidiscretize(semi,tspan)
 summary_callback = SummaryCallback()
 callbacks = CallbackSet(summary_callback)
 
 ###############################################################################
 # run the simulation
-
 sol = solve(ode, SSPRK43(), save_everystep=false, callback=callbacks)
