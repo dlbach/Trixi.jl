4901b6b161a36da673acc6614e8899ea553d775a
Daniel Bach <daniel.bach@uni-koeln.de>
2023-03-05 23:44:01 +0100

    added curved mesh divergence cleaning example

--- examples/structured_2d_dgsem/elixir_glm_maxwell_source_term.jl
+++ examples/structured_2d_dgsem/elixir_glm_maxwell_source_term.jl
@@ -4,9 +4,18 @@ using Trixi
 
 ###############################################################################
 # semidiscretization of the Maxwell equations
+"""
+f1(xi) = SVector(xi/sqrt(2), -sqrt( 1.0 - 0.5 * xi^2 ))
+f2(xi) = SVector(xi/sqrt(2), sqrt( 1.0 - 0.5 * xi^2 ))
+f3(eta) = SVector(-sqrt( 1.0 - 0.5 * eta^2 ), eta/sqrt(2))
+f4(eta) = SVector(sqrt( 1.0 - 0.5 * eta^2 ), eta/sqrt(2))
+"""
+f1(xi) = SVector(0.2*cos((xi+1.0)*pi),0.2*sin((xi+1.0)*pi))
+f2(xi) = SVector(cos((xi+1.0)*pi),sin((xi+1.0)*pi))
+f3(eta) = SVector(0.4*(eta+1.0)+0.2,0.0)
+f4(eta) = SVector(0.4*(eta+1.0)+0.2,0.0)
+cells_per_dimension = (10, 10)
 
-###############################################################################
-# semidiscretization of the Maxwell equations
 function boundary_condition_irradiation(u_inner, orientation_or_normal_direction, direction, x, t,
   surface_flux_function, equations::GLMMaxwellEquations2D)
   if iseven(direction)
@@ -16,30 +25,25 @@ function boundary_condition_irradiation(u_inner, orientation_or_normal_direction
   end
 end
 
-
-function boundary_value_function(x, t, equations::GLMMaxwellEquations2D)
-  return SVector(x[1], x[2], 0.0, 0.0)
-end
-
-
 function source_term_function(u, x, t, equations::GLMMaxwellEquations2D)
   return SVector(0.0, 0.0, 0.0, equations.c_h^2 * 2.0)
 end
 
 function initial_condition_zero(x, t, equations::GLMMaxwellEquations2D)
   if t > 0.0
-    return SVector(2.0*x[1], 0.0 , 0.0, 0.0)
+    return SVector(x[1], x[2], 0.0, 0.0)
   else 
     return SVector(0.0, 0.0, 0.0, 0.0)
   end
 end
 
 equation = GLMMaxwellEquations2D(2.0)
-boundary_conditions = (x_neg = Trixi.boundary_condition_perfect_conducting_wall,
-                       x_pos = Trixi.boundary_condition_perfect_conducting_wall,
-					   y_neg = BoundaryConditionDirichlet(boundary_value_function),
-					   y_pos = BoundaryConditionDirichlet(boundary_value_function))
-mesh = TreeMesh((-1.0, -1.0), (1.0, 1.0), initial_refinement_level=2, n_cells_max=10^4, periodicity = false)
+#boundary_conditions = Trixi.boundary_condition_perfect_conducting_wall
+boundary_conditions = (y_neg = Trixi.boundary_condition_periodic,
+                       y_pos = Trixi.boundary_condition_periodic,
+					   x_neg = Trixi.boundary_condition_perfect_conducting_wall,
+					   x_pos = Trixi.boundary_condition_perfect_conducting_wall)
+mesh = StructuredMesh(cells_per_dimension, (f1,f2,f3,f4), periodicity = (false, true))
 solver = DGSEM(polydeg = 3, surface_flux = Trixi.flux_upwind)
 semi = SemidiscretizationHyperbolic(mesh, equation, initial_condition_zero, solver,
                                     boundary_conditions = boundary_conditions, source_terms = source_term_function)
@@ -48,12 +52,12 @@ semi = SemidiscretizationHyperbolic(mesh, equation, initial_condition_zero, solv
 # ODE solvers, callbacks etc.
 
 
-analysis_interval = 100
+analysis_interval = 100000
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_analysis=true)
-save_solution_callback = SaveSolutionCallback(interval = 100, save_initial_solution=false, save_final_solution=true, output_directory="out")
+save_solution_callback = SaveSolutionCallback(interval = 100000, save_initial_solution=false, save_final_solution=true, output_directory="out")
 
-cfl = 1.0
-tspan = (0.0,1e-5)
+cfl = 0.05
+tspan = (0.0,1e-6)
 
 ode = semidiscretize(semi,tspan)
 summary_callback = SummaryCallback()
